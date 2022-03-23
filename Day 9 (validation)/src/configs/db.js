f0e6c66e690e688c7744544841f0b709a5836b48
Mayurwagh98 <mswagh1998@gmail.com>
2022-03-23 15:14:25 +0530

    validations

---
+++ Day 9 (validation)/src/configs/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+
+modeule.exports = () =>{
+
+    return mongoose.connect("mongodb+srv://MayurWagh:mayur_123@cluster0.47rnh.mongodb.net/web15-atlas?retryWrites=true&w=majority")
+}
